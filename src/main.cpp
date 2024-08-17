a130f08fcb4f7a82967abc74d6bdbbb0973481ee
Mazin Mustafa <musta132@ad.ahc.umn.edu>
2024-08-16 19:55:22 -0500

    update

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,23 @@ int main(){
     // test_engine_1d_transmission_line_S_parameters();
     // test_engine_1d_RCS_vertical_wire();
     // test_engine_1d_far_field_transmission_line();
-    test_engine_1d_near_field_vertical_dipole();
+    // test_engine_1d_near_field_vertical_dipole();
+
+
+
+
+    // const real_t y=-1.0;
+    // vector_t<real_t> r_m=vector_t<real_t>(-1.0, 0.0, +0.0);
+    // vector_t<real_t> e_1=vector_t<real_t>(+0.0, y, +1.0);
+    // vector_t<real_t> e_2=vector_t<real_t>(+0.0, y, -1.0);
+    // vector_t<real_t> r_p=vector_t<real_t>(+1.0, 0.0, +0.0);
+    // basis_2d_t b_n=basis_2d_t(r_m, e_1, e_2, r_p, 0, 0);
+    
+    // vector_t<real_t> l_n_m=0.5*(b_n.L_m[0]+b_n.L_m[1]);
+    // l_n_m = unit(l_n_m);
+    // real_t sign=l_n_m*b_n.n_p[0]<0.0 ? -1.0 : +1.0;
+    // real_t Omega=2.0*(pi+sign*acos(b_n.n_m[0]*b_n.n_p[0]));
+    // print(Omega/(4.0*pi));
 
     return 0;
 }
